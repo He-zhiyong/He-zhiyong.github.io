1461dcda3d75483cb03944acbf21cae7e657a28e
Hezhiyong <15988134176@163.com>
2018-06-25 14:31:02 +0800

    test pwa

--- README.md
+++
@@ -1,2 +0,0 @@
-# he-zhiyong.github.io
-何志勇(Hezhiyong)的个人主页
---
+++ precache-manifest.d5e3739b99831ee0e769f1f70c2b77ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c18b37adae501c7e1353350dcc8f2da",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "78913d5af663e1644e1b",
+    "url": "chunk-vendors.78913d5a.js"
+  },
+  {
+    "revision": "df0724791225f0d2531f",
+    "url": "app.df072479.js"
+  },
+  {
+    "revision": "df0724791225f0d2531f",
+    "url": "app.59de7b77.css"
+  }
+];
